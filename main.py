cdc6f5f6537891aa41fb5e82bbdbea13af96aa37
Daryl Fung <darylfung96@gmail.com>
2021-05-04 18:12:43 -0500

    added get missing and total samples

--- main.py
+++ main.py
@@ -13,9 +13,18 @@ from data_preprocessing import process_data
 from dataset import MMCDataset
 from model import LightningLSTM
 
+os.makedirs('lightning_logs', exist_ok=True)
 log_index = len(os.listdir('lightning_logs'))
 
-sorted_data, sorted_length, target_data = process_data()
+# can be LSTM or CNNLSTM
+model_type = "LSTM"
+# pad the patient samples to 6 time stamp in sequence, if we are using LSTM we do not have to use this
+# this is only for CNN, we could experiment LSTM with this variable set as True too
+pad_in_sequence = True
+
+if model_type == "CNNLSTM":
+    pad_in_sequence = True
+sorted_data, sorted_length, target_data = process_data(pad_in_sequence=pad_in_sequence)
 
 kf = KFold(n_splits=5)
 for index, (train_index, test_index) in enumerate(kf.split(sorted_data)):
@@ -36,9 +45,9 @@ for index, (train_index, test_index) in enumerate(kf.split(sorted_data)):
     train_data_loader = DataLoader(train_dataset, batch_size=64)
     test_data_loader = DataLoader(test_dataset, batch_size=64)
 
-    lightning_lstm = LightningLSTM(input_size=train_dataset[0][0].shape[1], hidden_size=32, output_size=train_dataset.y.shape[1])
+    lightning_lstm = LightningLSTM(model=model_type, input_size=train_dataset[0][0].shape[1], hidden_size=32, output_size=train_dataset.y.shape[1])
 
-    tb_logger = pl_loggers.TensorBoardLogger(f'lightning_logs/{log_index}')
+    tb_logger = pl_loggers.TensorBoardLogger(f'lightning_logs/{log_index}_{model_type}')
     pl_trainer = Trainer(max_epochs=100, callbacks=[EarlyStopping(monitor='validation_loss', patience=6)],
                                                     checkpoint_callback=ModelCheckpoint('saved_model/model', monitor='validation_loss',
                                                                                         save_top_k=1, prefix=f'kfold_{index}'),
--- model.py
+++ model.py
@@ -1,14 +1,17 @@
+from sklearn.metrics import f1_score, precision_score, recall_score
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import pytorch_lightning as pl
 
 
 class LightningLSTM(pl.LightningModule):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, model, input_size, hidden_size, output_size):
         super(LightningLSTM, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.model = LSTM(input_size, hidden_size, output_size)
+
+        self.model = model_dict[model](input_size, hidden_size, output_size)
         self.criterion = nn.CrossEntropyLoss()
 
     def on_train_start(self):
@@ -31,6 +34,15 @@ class LightningLSTM(pl.LightningModule):
 
         loss = self.criterion(out, torch.argmax(y, 1))
         self.log("validation_loss", loss.item(), prog_bar=True)
+
+        predictions = out.argmax(1).cpu().detach().numpy()
+        y_labels = y.argmax(1).cpu().detach().numpy()
+        f1 = f1_score(y_labels, predictions, average='micro')
+        precision = precision_score(y_labels, predictions, average='micro')
+        recall = recall_score(y_labels, predictions, average='micro')
+        self.log("validation f1", f1, prog_bar=True)
+        self.log("validation precision", precision, prog_bar=True)
+        self.log("validation recall", recall, prog_bar=True)
         return loss
 
     def configure_optimizers(self):
@@ -57,3 +69,33 @@ class LSTM(nn.Module):
         last_seq_idxs = torch.LongTensor([len_idx - 1 for len_idx in inputs_length])
         last_output = X[range(X.shape[0]), last_seq_idxs, :]
         return self.output_layer(last_output)
+
+
+class CNNLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(CNNLSTM, self).__init__()
+
+        self.conv1 = nn.Conv1d(input_size, 500, 2)
+        self.bn1 = nn.BatchNorm1d(500)
+        self.conv2 = nn.Conv1d(500, 500, 2)
+
+        self.lstm = nn.LSTM(input_size=500, hidden_size=hidden_size, batch_first=True)
+        self.output_layer = nn.Sequential(nn.Dropout(0.5),
+                                          nn.Linear(hidden_size, hidden_size), nn.ReLU(), nn.Dropout(0.5),
+                                          nn.Linear(hidden_size, hidden_size), nn.ReLU(), nn.Dropout(0.5),
+                                          nn.Linear(hidden_size, hidden_size), nn.ReLU(), nn.Dropout(0.5),
+                                          nn.Linear(hidden_size, output_size))
+
+    def forward(self, inputs, inputs_length):
+        conv1_output = F.relu(self.bn1(self.conv1(inputs.transpose(1, 2))))
+        conv2_output = F.relu(self.conv2(conv1_output))
+
+        out, hidden = self.lstm(conv2_output.transpose(1, 2))
+        last_output = out[:, -1, :]
+        return self.output_layer(last_output)
+
+
+model_dict = {
+    'LSTM': LSTM,
+    'CNNLSTM': CNNLSTM
+}
