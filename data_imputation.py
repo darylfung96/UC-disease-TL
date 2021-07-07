3a88c7cdd95697bc7958bfbe31ebc104b2063b1a
Daryl Fung <darylfung96@gmail.com>
2021-07-07 12:35:44 -0500

    fix shape

--- data_imputation.py
+++ data_imputation.py
@@ -9,8 +9,8 @@ from GAIN.gain import GAIN
 output_dict = process_data(pad_in_sequence=True)
 sorted_data, sorted_length, target_data, missing_data = output_dict['sorted_data'], output_dict['sorted_length'], \
                                                         output_dict['target_data'], output_dict['missing_data']
-sorted_data = sorted_data.reshape(sorted_data.shape[0], -1)
-missing_data = missing_data.reshape(missing_data.shape[0], -1)
+sorted_data = sorted_data.reshape(-1, sorted_data.shape[-1])
+missing_data = missing_data.reshape(-1, missing_data.shape[-1])
 
 dataset = MMCDataset(sorted_data, sorted_length, target_data)
 train_data_loader = DataLoader(dataset)
