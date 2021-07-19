722575e3de9704c7943f0b2f1a6be0b6cbf6329f
Daryl Fung <darylfung96@gmail.com>
2021-07-19 18:37:01 -0500

    changed batch size

--- GAIN/gain.py
+++ GAIN/gain.py
@@ -77,7 +77,7 @@ class GAIN(pl.LightningModule):
 
         # %% System Parameters
         # 1. Mini batch size
-        self.mb_size = 128
+        self.mb_size = 32
         # 2. Missing rate
         self.p_miss = 0.2
         # 3. Hint rate
